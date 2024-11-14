087f79b209936986347f47bdc2876566845541e8
erwan-laudic <erwan.laudic@edu.esiee.fr>
2024-11-14 10:07:15 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
+"""nombre premier"""
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
+    """isprime"""
+    if p == 1 :
+        return False
+    for i in range(2,int(sqrt(p)+1)):
+        if p % i == 0:
+            return False
+    return True
 
-    # votre code ici
-
-    pass
 
 #### Fonction principale
 
-
 def main():
-
+    """main"""
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
-    print()
+    print("108 est premier :", isprime(108))
 
 
 if __name__ == "__main__":
